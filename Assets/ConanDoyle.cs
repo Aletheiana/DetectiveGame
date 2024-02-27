19334792b808510bb814852380f518ba35545244
Maddy Letowska <me.letowska@gmail.com>
2024-02-27 01:10:51 +0000

    dialogue box displays a series of lines for its assigned character

--- Assets/ConanDoyle.cs
+++ Assets/ConanDoyle.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConanDoyle : MonoBehaviour
 {
+
+    public string[] Sherlock = new string[100];
+    public string[] Watson = new string[100];
+    public string[] Gregson = new string[100];
+    public string[] Lestrade = new string[100];
+    public List<string[]> words; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        words = new List<string[]>() { Sherlock, Gregson, Watson, Lestrade };
+        this.Sherlock[0] = "Sherlock";
+        this.Sherlock[1] = "Hello World";
+        this.Sherlock[2] = "Hello me";
+        this.Sherlock[3] = "Hello Watson";
+        this.Sherlock[4] = "Thanks for playing";
+        this.Watson[0] = "Watson";
+        this.Watson[1] = "Why are you reading my diary?";
+        this.Gregson[0] = "Gregson";
+        this.Gregson[1] = "Welcome to Scotland Yard";
+        this.Lestrade[0] = "Lestrade";
+        this.Lestrade[1] = "Rachel";
     }
 
     // Update is called once per frame
@@ -16,7 +35,7 @@ public class ConanDoyle : MonoBehaviour
         
     }
 
-    public string DialogueOne = "Hello World";
-    public string DialogueTwo = "Hello Me";
-    public string DialogueThree = "Thanks for playing";
+
+    
+    
 }
--- Assets/SpeechBox.cs
+++ Assets/SpeechBox.cs
@@ -3,28 +3,47 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpeechBox : MonoBehaviour
 {
 
     public ConanDoyle Arthur;
-    public int Character;
-    public TMP_Text MyLines;
+    public string MyName = "Sherlock";
+    public string[] MyLines;
+    public TMP_Text MyMouth;
     public Canvas canvas;
+    public int linecount;
 
     // Start is called before the first frame update
     void Start()
     {
         // Finds the location of this character's dialogue for this scene
         Arthur = FindObjectOfType<ConanDoyle>();
-        // Finds the text object which will display this character's text
-        MyLines.text = Arthur.DialogueOne;
+        MyLines = Arthur.Sherlock;
+        List<string[]> lines = Arthur.words;
+        foreach (string[]  line in lines)
+        {
+            if (line[0] ==  MyName)
+            {
+                MyLines = line;
+                //Debug.Log("Accepted " + line[0]);
+            }
+            else
+            {
+                //Debug.Log("Rejected " + line[0]);
+            }
+        }
+        // Finds (doesn't really find) the text object and displays this character's first line of text
+        linecount = 0;
+        MyMouth.text = MyLines[linecount];
+        linecount++;
         // Moves the dialogue box into the correct position and size relative to the canvas
         RepositionLeft();
         // Moves the box's text to line up with the box
         RepositionText();
-        //Debug.Log("Offsetmax: " + MyLines.GetComponent<RectTransform>().offsetMax);
+        //Debug.Log("Offsetmax: " + MyMouth.GetComponent<RectTransform>().offsetMax);
     }
 
     // Update is called once per frame
@@ -33,7 +52,8 @@ public class SpeechBox : MonoBehaviour
         if (Input.GetMouseButtonDown(0) == true)
         {
             Debug.Log("Left-clicked on dialogue");
-            MyLines.text = Arthur.DialogueTwo;
+            MyMouth.text = MyLines[linecount];
+            linecount++;
         }
         if (Input.GetMouseButtonDown(1) == true)
         {
@@ -64,16 +84,16 @@ public class SpeechBox : MonoBehaviour
         // Converts the stretch into appropriate format
         Vector2 TopRight = new Vector2(rightstretch, topstretch);
         Vector2 BottomLeft = new Vector2(borderstretch, borderstretch);
-        Debug.Log("before transform, OffsetMax: " + this.GetComponent<RectTransform>().offsetMax + ", OffsetMin: " + this.GetComponent<RectTransform>().offsetMin);
+        //Debug.Log("before transform, OffsetMax: " + this.GetComponent<RectTransform>().offsetMax + ", OffsetMin: " + this.GetComponent<RectTransform>().offsetMin);
         // Transforms the dialogue box
         this.GetComponent<RectTransform>().offsetMax = TopRight;
         this.GetComponent<RectTransform>().offsetMin = BottomLeft;
-        Debug.Log("after transform, OffsetMax: " + this.GetComponent<RectTransform>().offsetMax + ", OffsetMin: " + this.GetComponent<RectTransform>().offsetMin);
+        //Debug.Log("after transform, OffsetMax: " + this.GetComponent<RectTransform>().offsetMax + ", OffsetMin: " + this.GetComponent<RectTransform>().offsetMin);
         
     }
 
     // Moves the box's text to line up with the box
-    // Would do "MyLines.GetComponent<RectTransform>().rect = this.GetComponent<RectTransform>().rect;" 
+    // Would do "MyMouth.GetComponent<RectTransform>().rect = this.GetComponent<RectTransform>().rect;" 
     // But can't cause is read-only
     private void RepositionText()
     {
@@ -86,23 +106,23 @@ public class SpeechBox : MonoBehaviour
         Vector2 TextTopCorner = new Vector2(width, height);
         Vector2 TextBottomCorner = new Vector2(20, 20);
         // Transforms the text's size
-        MyLines.GetComponent<RectTransform>().offsetMax = TextTopCorner;
-        MyLines.GetComponent<RectTransform>().offsetMin= TextBottomCorner;
+        MyMouth.GetComponent<RectTransform>().offsetMax = TextTopCorner;
+        MyMouth.GetComponent<RectTransform>().offsetMin= TextBottomCorner;
         // Repositions the text relative to canvas
         float canvasWidth = canvas.GetComponent<RectTransform>().rect.width;
         float canvasHeight = canvas.GetComponent<RectTransform>().rect.height;
         canvasWidth = canvasWidth / 2;
         canvasHeight = canvasHeight / 2;
         //Debug.Log("Canvas dimensions: " + canvasWidth + " x " + canvasHeight);
-        float textWidth = MyLines.GetComponent<RectTransform>().rect.width;
-        float textHeight = MyLines.GetComponent<RectTransform>().rect.height;
+        float textWidth = MyMouth.GetComponent<RectTransform>().rect.width;
+        float textHeight = MyMouth.GetComponent<RectTransform>().rect.height;
         textWidth = textWidth / 2;
         textHeight = textHeight / 2;
         float x = 0 - (canvasWidth - textWidth - 20);
         float y = 0 - (canvasHeight - textHeight - 20);
         Vector2 textCenter = new Vector2(x, y);
-        Debug.Log("Text Center: (" + x + ", " + y + ")");
-        MyLines.GetComponent<RectTransform>().anchoredPosition = textCenter;
+        //Debug.Log("Text Center: (" + x + ", " + y + ")");
+        MyMouth.GetComponent<RectTransform>().anchoredPosition = textCenter;
     }
 
     
